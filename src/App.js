5ef7472ae734a3e010f3da6215bbb951fe2ea4d4
reactayesa3 <react3@itweblearning.com>
2022-04-19 15:45:47 +0200

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import logo from './img/logo.svg';
 import './App.css';
 
 function App() {
+
+  const message = 'Hola Mundo!';
+  const urlPic = 'https://i.ibb.co/HH9yRts/2.jpg';
+  const footer = <footer>
+    <p>&copy; 2022 Todos los derechos reservados</p>
+  </footer>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Emplearemos JSX
+    <div className='container'>
+      {/* Los comentarios en JSX también van en interpolación */}
+      <h1>{message}</h1>
+      <p>Lorem ipsumm</p>
+      {/* Los estilos en línea han de introducirse con un objeto */}
+      <img src={logo} style={{width: '240px'}} alt="Logotipo de React"/>
+      <img src={urlPic} alt="Imagen demo" />
+      {footer}
     </div>
   );
 }
